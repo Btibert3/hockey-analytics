a79c741bbef63f75eb3642b2416157e2e09828f2
Brock Tibert <btibert3@gmail.com>
2022-03-29 20:45:26 -0400

    bump

--- hockey/__init__.py
+++ hockey/__init__.py
@@ -3,6 +3,6 @@
 # change to calendar versioning of sorts 
 # YYYY.MMDDv where v is 1 based within the month/day combo
 # TODO:  follow best practices here
-__version__ = '2022.03291'
+__version__ = '2022.03292'
 
 from .viz import *
